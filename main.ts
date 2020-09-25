0586fbaf91b446b55a0ea8218641ab6d6a867a1e
matt-visser <71887504+matt-visser@users.noreply.github.com>
2020-09-25 10:43:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,7 @@ input.buttonA.onEvent(ButtonEvent.Click, function () {
 })
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
+    light.showRing(
+    `white white white white white white white white white white`
+    )
 })
