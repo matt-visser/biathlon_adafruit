696db67cf6ecf7bf6c33929a64d2a05e5dc28a87
matt-visser <71887504+matt-visser@users.noreply.github.com>
2020-09-25 10:25:11 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+	
+})
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
 })
