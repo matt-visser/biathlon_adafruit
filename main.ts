95a5211886c2d2ea87c994584f2414f50e6b7548
nick416tor <71888351+nick416tor@users.noreply.github.com>
2020-09-25 10:40:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     light.showAnimation(light.runningLightsAnimation, 500)
+    makerController.player1.press(ArcadeButton.A)
+    if (true) {
+    	
+    } else {
+    	
+    }
 })
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
